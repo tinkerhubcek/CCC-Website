dbbfe8dca26a4eb508290225f12271b7b50d582a
athul <athul8720@gmail.com>
2019-10-09 23:16:56 +0530

    Fixes

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -18,16 +18,6 @@ export default function({ data }) {
             />
             <div className="container">
                 <article className="blog-post">
-                    {data.markdownRemark.frontmatter.banner != null && (
-                        <div className="banner">
-                            <Img
-                                fluid={
-                                    data.markdownRemark.frontmatter.banner
-                                        .childImageSharp.fluid
-                                }
-                            />
-                        </div>
-                    )}
                     <div className="head text-primary">
                         <h1>{data.markdownRemark.frontmatter.title}</h1>
                         <p className="post-date">
